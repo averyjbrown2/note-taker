9a748686a9e248c22ecba76b8a6055a9e8e1d40f
Avery Brown-Gathings <72925420+averyjbrown2@users.noreply.github.com>
2020-12-05 17:53:45 -0500

    added script data

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var fs = require("fs");
 var db = require("./db/db.json")
 var app = express();
 var PORT = process.env.PORT ? process.env.PORT : 3000;
-
+var index = require("index.")
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,7 +13,8 @@ app.use(express.json());
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
+//catch-all for any additions to the base html to send user to homepage
+app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
   });
 
