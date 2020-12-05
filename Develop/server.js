849840b721fccf390c5123029eae4dc5564acfb6
Avery Brown-Gathings <72925420+averyjbrown2@users.noreply.github.com>
2020-12-05 15:43:53 -0500

    altered path to html files

--- Develop/server.js
+++ Develop/server.js
@@ -14,11 +14,11 @@ app.use(express.json());
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
   });
 
 app.get("/notes", function(req, res) {
-res.sendFile(path.join(__dirname, "./notes.html"));
+res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.post("/api/notes", function(req, res){
